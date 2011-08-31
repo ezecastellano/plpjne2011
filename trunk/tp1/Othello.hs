bce1f54479eb66648b24e4801aa536e26b6c38ec
ezequiel.castellano@gmail.com <ezequiel.castellano@gmail.com@4ed97d2f-9156-680e-8256-23290e22533e>
2011-08-31 02:58:32 +0000

    Ejercicio 9 y 10, pero el 10 no está bien, hay que usar foldArbol!

--- trunk/tp1/Othello.hs
+++ trunk/tp1/Othello.hs
@@ -48,3 +48,17 @@ posiblesBuenas :: Juego -> [(Jugada,Juego)]
 posiblesBuenas j = [ ((M (c1,f1)),fromJust (jugar (M (c1,f1)) j)) | c1<-['a'..'h'], f1<-[1..8], not ( isNothing (jugar (M (c1,f1)) j) )]
 
 -- Ejercicio 9
+
+foldArbol :: (a->[b]->b) -> Arbol a -> b
+foldArbol f (Nodo a ts) = f a (map (foldArbol f) ts)
+
+-- Ejercicio 10 (Este ejercicio está mal porque nos dice que usemos foldArbol... esto se parece a un fold  "Nodo a (map (\x ->podar' x (n-1)) xs)" )
+
+podar :: Int -> Arbol a -> Arbol a
+podar n a = podar' a (n-1)
+
+-- foldArbol (\x rs -> podar' a (n-1)) a
+
+podar' :: Arbol a -> Int -> Arbol a
+podar' (Nodo a xs) n  =  if n > 0 && not( genericLength xs == 0) then Nodo a (map (\x ->podar' x (n-1)) xs)  else (Nodo a [])
+ 
