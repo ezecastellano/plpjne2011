00ab773bb011cde122243b8c861c892a29a85e75
carolinahadad <carolinahadad@4ed97d2f-9156-680e-8256-23290e22533e>
2011-10-17 03:21:28 +0000

    hasta el 3

--- tp2/src/solucion.pl
+++ tp2/src/solucion.pl
@@ -6,17 +6,41 @@
 % EJ 1
 % tamano(?Tipo, ?Tam)
 
+tamano(objetivo, tam(2,2)).
+tamano(unidad, tam(1,1)).
+tamano(horizontal, tam(1,2)).
+tamano(vertical, tam(2,1)).
+
 
 % EJ 2
 % movimiento_posible(+Tipo, -Dir)
 
+horizontal_direcciones(este).
+horizontal_direcciones(oeste).
+
+vertical_direcciones(norte).
+vertical_direcciones(sur).
+
+todas_direcciones(T):- vertical_direcciones(T).
+todas_direcciones(T):- horizontal_direcciones(T).
+
+movimiento_posible(objetivo, T) :- todas_direcciones(T).
+movimiento_posible(unidad, T):- todas_direcciones(T).
+movimiento_posible(horizontal, T) :- horizontal_direcciones(T).
+movimiento_posible(vertical, T) :- vertical_direcciones(T).
+
 
 % EJ 3
 % mover(+Pos, +Dir, -Pos)
+mover(pos(A,B), norte, T) :- T = pos(X,Y), B = Y, X is A-1.
+mover(pos(A,B), sur, T) :- T = pos(X,Y), B = Y, X is A+1.
+mover(pos(A,B), este, T) :- T = pos(X,Y), A = X, Y is B+1.
+mover(pos(A,B), este, T) :- T = pos(X,Y), A = X, Y is B-1.
 
 
 % EJ 4
 % en_tablero(+Tablero, ?Pos)
+%en_tablero(T, P) :- T = tablero(TAM, _ , _), TAM = tam(A,B), P = pos(X,Y), X > 0, Y> 0, X< MAXX, Y< MAXY, MAXX is A+1, MAXY is B+1.
 
 
 % EJ 5
