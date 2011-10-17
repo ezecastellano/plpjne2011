6f1f09f4f15530b2900efe49057364e98245cbcd
carolinahadad <carolinahadad@4ed97d2f-9156-680e-8256-23290e22533e>
2011-10-17 01:54:58 +0000

    fuentes y enunciado del tp de prolog

---
+++ tp2/src/solucion.pl
@@ -0,0 +1,49 @@
+
+%% Carga los módulos test y mostrar.
+?- [tests].
+?- [mostrar].
+
+% EJ 1
+% tamano(?Tipo, ?Tam)
+
+
+% EJ 2
+% movimiento_posible(+Tipo, -Dir)
+
+
+% EJ 3
+% mover(+Pos, +Dir, -Pos)
+
+
+% EJ 4
+% en_tablero(+Tablero, ?Pos)
+
+
+% EJ 5
+% pieza_ocupa(+Pieza, -Pos)
+
+
+% EJ 6
+% quitar(?X, +L, -R)
+
+
+% EJ 7
+% movimiento_valido(+Tablero, -Pieza, -Dir)
+
+
+% EJ 8
+% mover_pieza(+Tablero1, +Pieza, +Dir, -Tablero2)
+
+
+% EJ 9
+% resolver(+Tablero, -Movimientos, -TableroFinal)
+
+
+% EJ 10
+% armar_tablerosA(?Tablero)
+
+
+% EJ 11
+% armar_tablerosB(?Tablero)
+
+
