2bac296eb7d9860261f6ce8dec89348c72535d57
marianosoy@gmail.com <marianosoy@gmail.com@4ed97d2f-9156-680e-8256-23290e22533e>
2011-10-26 20:36:28 +0000

    terminado!

--- trunk/tp2/src/solucion.pl
+++ trunk/tp2/src/solucion.pl
@@ -56,28 +56,29 @@ quitar(X,L,LsinX):- append(L1,L2A,L), L2A =[A|L2], X=A, append(L1,L2,LsinX).
 % EJ 7
 % movimiento_valido(+Tablero, -Pieza, -Dir)
 movimiento_valido(T1, Pieza, Dir):- 
-%Pieza es una pieza del tablero
+% #Pieza es una pieza del tablero
 	T1 = tablero(_,_,Piezas),
 	member(Pieza,Piezas),
 	Pieza = pieza(TipoPieza, PosVieja),
-%Pieza se puede mover en dicha direccion
+% #Pieza se puede mover en dicha direccion
 	movimiento_posible(TipoPieza, Dir),
 	mover(PosVieja, Dir, PosNueva),
-%Su nueva posicion esta en el tablero
+% #Su nueva posicion esta en el tablero
 	PiezaNueva = pieza(TipoPieza, PosNueva),
 	pieza_en_tablero(T1, PiezaNueva),
-%Su nueva posicion no se superpone con otra pieza
-	DemasPiezas = quitar(Pieza, Piezas),
+% #Su nueva posicion no se superpone con otra pieza
+	quitar(Pieza, Piezas, DemasPiezas),
 	no_superponen_entre_piezas(DemasPiezas, PiezaNueva).
 
 % pieza_en_tablero(+T, +PiezaNueva)
 pieza_en_tablero(T, PiezaNueva):-
 	forall(pieza_ocupa(PiezaNueva, PosNuevaOcupada), en_tablero(T, PosNuevaOcupada)).
 
-%Espera que PiezaNueva no este en piezas
-%no_superponen_entre_piezas(+Piezas, +PiezaNueva)
+% #Espera que PiezaNueva no este en piezas
+% no_superponen_entre_piezas(+Piezas, +PiezaNueva)
 no_superponen_entre_piezas(Piezas, PiezaNueva):-
-	forall( pieza_ocupa(PiezaNueva, PosOcupa), forall( member(P,Piezas), forall(pieza_ocupa(P, PosOcupaP),	PosOcupaP \= PosOcupa))).
+	forall( member(P,Piezas), forall(pieza_ocupa(P, PosOcupaP),forall( pieza_ocupa(PiezaNueva, PosOcupa), PosOcupaP \= PosOcupa))).
+	
 
 % EJ 8
 % mover_pieza(+Tablero1, +Pieza, +Dir, -Tablero2)
@@ -113,14 +114,58 @@ resolverParametro(TableroInicial, TablerosViejos, [(Pieza, Dir)| Movimientos], T
 
 % EJ 10
 % armar_tablerosA(?Tablero)
+armar_tablerosA(tablero(Dim, Pos, Piezas)):-
+	ubicar_piezas_en_tablero(tablero(Dim, Pos, []), Piezas, T2),
+	resoluble(T2),
+	mostrar(T2).
+	
 
+% resoluble(+Tablero)
+resoluble(T):-
+	resolver(T, _, _),!.
+
+% devuelve el tablero recibido con las piezas instanciadas.
+% generando todas las combinaciones
+% (dentro del tablero y sin superponerse)
+% ubicar_piezas_en_tablero(+Tablero, ?Piezas, -Tablero)
+ubicar_piezas_en_tablero(T1, [], T1).
+ubicar_piezas_en_tablero(T1, [Pieza|Piezas], T2) :-
+	poner_si_valida(T1, Pieza, T3),
+	ubicar_piezas_en_tablero(T3, Piezas, T2).
+
+% pone en TF la pieza si es valida.
+% poner_si_valida(+T1, ?Pieza, -TF)
+poner_si_valida(T1, Pieza, TF):-
+	T1 = tablero(Dim, Pos, PiezasOriginales),
+	Pieza = pieza(_,Posicion),
+	en_tablero(T1, Posicion),
+	pieza_en_tablero(T1, Pieza),
+	no_superponen_entre_piezas(PiezasOriginales, Pieza),
+	TF = tablero(Dim, Pos, [Pieza|PiezasOriginales]).
 
 % EJ 11
 % armar_tablerosB(?Tablero)
+armar_tablerosB(tablero(Dim, Pos, Piezas)):-
+	ubicar_piezas_en_tableroB(tablero(Dim, Pos, []), Piezas, T2),
+	mostrar(T2).
 
-%mover_pieza(tablero(tam(4, 4), pos(3, 3), [pieza(objetivo, pos(1, 3)), pieza(vertical, pos(3,2))]), pieza(objetivo , pos(1, 3)), sur , T). Anda OK
-%problema(t0, Tablero), resolver(Tablero, _, Final),mostrar(Tablero), mostrar(Final).
-%movimiento_valido(tablero(tam(4, 4), pos(3, 3), [pieza(objetivo, pos(1, 3)), pieza(vertical, pos(3,2))]), pieza(objetivo, pos(1, 3)), sur), mostrar(tablero(tam(4, 4), pos(3, 3), [pieza(objetivo, pos(1, 3)), pieza(vertical, pos(3,2))])).
-%pieza_en_tablero(tablero(tam(4, 4), pos(3, 3), [pieza(objetivo, pos(1, 3)), pieza(vertical, pos(3,2))]), pieza(objetivo, pos(1,4)))
-%problema(t0, Tablero), resolver(Tablero, _, Final), mostrar(Tablero), mostrar(Final).
-%problema(t0, T0), mover_pieza(T0, pieza(objetivo , pos(3, 1)), sur , T).
+
+% ubicar_piezas_en_tableroB(+Tablero, +Piezas, -Tablero)
+ubicar_piezas_en_tableroB(T1, [], T1).
+ubicar_piezas_en_tableroB(T1, [Pieza|Piezas], T2) :-
+	poner_si_valida(T1, Pieza, T3),
+	resoluble(T3),
+	ubicar_piezas_en_tableroB(T3, Piezas, T2).
+	
+
+% Tests:
+% mover_pieza(tablero(tam(4, 4), pos(3, 3), [pieza(objetivo, pos(1, 3)), pieza(vertical, pos(3,2))]), pieza(objetivo , pos(1, 3)), sur , T). Anda OK
+% problema(t0, Tablero), resolver(Tablero, _, Final),mostrar(Tablero), mostrar(Final).
+% movimiento_valido(tablero(tam(4, 4), pos(3, 3), [pieza(objetivo, pos(1, 3)), pieza(vertical, pos(3,2))]), pieza(objetivo, pos(1, 3)), sur), mostrar(tablero(tam(4, 4), pos(3, 3), [pieza(objetivo, pos(1, 3)), pieza(vertical, pos(3,2))])).
+% pieza_en_tablero(tablero(tam(4, 4), pos(3, 3), [pieza(objetivo, pos(1, 3)), pieza(vertical, pos(3,2))]), pieza(objetivo, pos(1,4)))
+% problema(t0, T0), mover_pieza(T0, pieza(objetivo , pos(3, 1)), sur , T).
+% problema(t0, Tablero), resolver(Tablero, _, Final), mostrar(Tablero), mostrar(Final).
+% resolver(tablero(tam(2,3),pos(1,2), [pieza(objetivo, pos(1,1)), pieza(vertical, pos(1,3))]), MS, TF), mostrar(TF).
+% armar_tablerosA(tablero(tam(4, 4), pos(3, 3), [pieza(objetivo, pos(1, 3)), pieza(vertical, pos(3,2))]), pieza(objetivo , pos(1, 3))).
+% armar_tablerosA(tablero(tam(2,4),pos(1,2), [pieza(objetivo, O), pieza(unidad, P), pieza(unidad, P2)])).
+
